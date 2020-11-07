1e2b3c667dd76540f944262ea57d74182d9b9dca
AaptiRidhiJain <72203592+AaptiRidhiJain@users.noreply.github.com>
2020-11-07 15:01:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    Matter.Body.setStatic(packageSprite,false);
+    Matter.Body.setStatic(packageBody,false);
   }
 }
 
